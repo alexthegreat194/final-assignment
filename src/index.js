e7a183bf3bf13dd136c42e6c3f3733d1f3771d5e
alexthegreat194 <alexander.gentry@aol.com>
2023-10-03 19:07:01 -0700

    main code

---
+++ src/index.js
@@ -0,0 +1,77 @@
+console.log("Start...");
+var CodeTypes;
+(function (CodeTypes) {
+    CodeTypes[CodeTypes["Numbers"] = 0] = "Numbers";
+    CodeTypes[CodeTypes["Letters"] = 1] = "Letters";
+    CodeTypes[CodeTypes["NumbersAndLetters"] = 2] = "NumbersAndLetters";
+})(CodeTypes || (CodeTypes = {}));
+var letters = Array.from('abcdefghijklmnopqrstuvwxyz');
+var numbers = Array.from('0123456789');
+var CodeGenerator = /** @class */ (function () {
+    function CodeGenerator(type, length) {
+        if (length === void 0) { length = 6; }
+        this.type = type;
+        this.length = length;
+    }
+    CodeGenerator.prototype.generate = function () {
+        var result = '';
+        switch (this.type) {
+            case CodeTypes.Numbers:
+                for (var i = 0; i < this.length; i += 1) {
+                    result += numbers[Math.floor(Math.random() * numbers.length)];
+                }
+                break;
+            case CodeTypes.Letters:
+                for (var i = 0; i < this.length; i += 1) {
+                    result += letters[Math.floor(Math.random() * letters.length)];
+                }
+                break;
+            case CodeTypes.NumbersAndLetters:
+                for (var i = 0; i < this.length; i += 1) {
+                    if (Math.random() < 0.5) {
+                        result += numbers[Math.floor(Math.random() * numbers.length)];
+                    }
+                    else {
+                        result += letters[Math.floor(Math.random() * letters.length)];
+                    }
+                }
+                break;
+        }
+        return result;
+    };
+    CodeGenerator.prototype.validate = function (input) {
+        var arr = Array.from(input);
+        switch (this.type) {
+            case CodeTypes.Numbers:
+                for (var i = 0; i < input.length; i++) {
+                    if (!numbers.includes(arr[i])) {
+                        return false;
+                    }
+                }
+                break;
+            case CodeTypes.Letters:
+                for (var i = 0; i < input.length; i++) {
+                    if (!letters.includes(arr[i])) {
+                        return false;
+                    }
+                }
+                break;
+            case CodeTypes.NumbersAndLetters:
+                for (var i = 0; i < input.length; i++) {
+                    if (!letters.includes(arr[i]) && !numbers.includes(arr[i])) {
+                        // console.log(arr[i])
+                        return false;
+                    }
+                }
+                break;
+        }
+        return true;
+    };
+    return CodeGenerator;
+}());
+var g = new CodeGenerator(CodeTypes.NumbersAndLetters);
+for (var i = 0; i < 10; i += 1) {
+    var code = g.generate();
+    console.log(code);
+    console.log(g.validate(code));
+}
---
+++ src/index.ts
@@ -0,0 +1,82 @@
+console.log("Start...")
+
+enum CodeTypes {
+    Numbers,
+    Letters,
+    NumbersAndLetters
+}
+
+const letters = Array.from('abcdefghijklmnopqrstuvwxyz');
+const numbers = Array.from('0123456789');
+
+class CodeGenerator {
+    type: CodeTypes;
+    length: number;
+
+    constructor(type: CodeTypes, length: number = 6) {
+        this.type = type;
+        this.length = length;
+    }
+
+    generate(): string {
+        let result = '';
+
+        switch(this.type) {
+            case CodeTypes.Numbers:
+                for (let i = 0; i < this.length; i+=1) {
+                    result += numbers[Math.floor(Math.random() * numbers.length)];
+                }
+                break; 
+
+            case CodeTypes.Letters:
+                for (let i = 0; i < this.length; i+=1) {
+                    result += letters[Math.floor(Math.random() * letters.length)];
+                }
+                break; 
+
+            case CodeTypes.NumbersAndLetters:
+                for (let i = 0; i < this.length; i+=1) {
+                    if (Math.random() < 0.5) {
+                        result += numbers[Math.floor(Math.random() * numbers.length)];
+                    } else {
+                        result += letters[Math.floor(Math.random() * letters.length)];
+                    }
+                }
+                break; 
+        }
+
+        return result;
+    }
+
+    validate(input:string) {
+        let arr: string[] = Array.from(input)
+        switch(this.type) {
+            case CodeTypes.Numbers:
+                for (let i = 0; i < input.length; i++) {
+                    if (!numbers.includes(arr[i])) { return false }
+                }
+                break;
+            case CodeTypes.Letters:
+                for (let i = 0; i < input.length; i++) {
+                    if (!letters.includes(arr[i])) { return false }
+                }
+                break;
+            case CodeTypes.NumbersAndLetters:
+                for (let i = 0; i < input.length; i++) {
+                    if ( !letters.includes(arr[i]) && !numbers.includes(arr[i]) )  { 
+                        // console.log(arr[i])
+                        return false 
+                    }
+                }
+                break;
+        }
+        return true
+    }
+}
+
+const g = new CodeGenerator(CodeTypes.NumbersAndLetters)
+for (let i = 0; i < 10; i+=1) {
+    let code = g.generate()
+    console.log(code)
+    console.log(g.validate(code))
+}
